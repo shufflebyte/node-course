50e377de990c9d009a760226f72b9b891eae58e6
Florian Soulier <florian.soulier@posteo.de>
2020-07-25 20:55:24 +0200

    Change: move routes to router files

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,74 +1,17 @@
 const express = require('express');
 require('./db/mongoose');
 
-const User = require('./models/user');
-const Task = require('./models/task')
+const { Router } = require('express');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 // PORT needed for heroku
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users", (req, res) => {
- //   console.log(req.body);
- //   res.send('testing');
-
-    const user = new User(req.body);
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((error) => {
-        res.status(400).send(error);
-    })
-});
-
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users);
-    }).catch(() => {
-        res.status(500).send();
-    });
-});
-
-app.get("/users/:id", (req, res) => {
-    console.log(req.params);
-    User.findById(req.params.id).then((user) => {
-        if (!user) {
-            return res.status(404).send();
-        } 
-        res.status(201).send(user);
-    }).catch((err) => {
-        res.status(500).send();
-    })
-});
-
-app.post("/tasks", (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch((error) => {
-        res.status(400).send(error);
-    });
-});
-
-app.get("/tasks", (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.status(201).send(tasks);
-    }).catch((error) => {
-        res.status(400).send(error);
-    });
-});
-
-app.get("/tasks/:id", (req, res) => {
-    Task.findById(req.params.id).then((task) => {
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.status(201).send(task);
-    }).catch((error) => {
-        res.status(500).send(error);
-    });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log("Started server on port " + port);
