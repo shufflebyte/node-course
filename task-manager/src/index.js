5385b04cf4c3871a4bbc406995d37316a18bc28e
Florian Soulier <florian.soulier@posteo.de>
2020-07-29 11:53:55 +0200

    Add: avatar file upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -27,6 +27,40 @@ const port = process.env.PORT || 3000;
 //     res.status(503).send('Site is under maintenance');
 // });
 
+// file upload with multer
+// const multer = require('multer');
+
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000   // 1 MByte
+//     },
+//     fileFilter(req, file, cb) {
+//         // cb(new Error('File must be a PDF'));
+//         // cb(undefined, true);
+//         // cb(undefined, false);
+
+//         //if (!file.originalname.endsWith('.pdf')) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('Please only upload Word docs'));
+//         }
+//         cb(undefined, true);
+//     }
+// });
+
+
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send();
+// }, (error, req, res, next) => {
+//     res.status(400).send({error: error.message});
+// });
+
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send();
+// });
+
+// end file upload ausprobieren
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
