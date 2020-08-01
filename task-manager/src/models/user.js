1c0db7ed59e74b9ea070bf49b9850d5d40da8d6c
Florian Soulier <florian.soulier@posteo.de>
2020-08-01 11:18:58 +0200

    Fix: missing await for async save function added

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -67,7 +67,7 @@ userSchema.methods.generateAuthToken = async function() {
     const user = this;
     const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET);
     user.tokens = user.tokens.concat({ token });
-    user.save();
+    await user.save();
     return token;
 };
 
