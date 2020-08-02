560dd674b05cd360c77f0a96f9deff267a4d8ef3
Florian Soulier <florian.soulier@posteo.de>
2020-08-02 13:23:35 +0200

    Add: share geolocation and basic ACK

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,26 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage', message, (msg) => {
+        console.log("message has been sent to server", msg);
+    });
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-    socket.emit('increment');
-})
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+    // does not support promises..
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    });
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -15,24 +15,29 @@ const pubDirPath = path.join(__dirname, '../public');
 app.use(express.static(pubDirPath));
 
 
-let count = 0; 
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
 
-    socket.emit('countUpdated', count);
+    // this only sends to requesting connection
+    socket.emit('message', 'Welcome!');
 
-    socket.on('increment', () => {
-        count++;
-        //only sends to the requesting client..
-        //socket.emit('countUpdated', count);
+    // broadcast to all connections except sender
+    socket.broadcast.emit('message', 'A new user has joined');
 
-        // broadcasting to all clients
-        io.emit('countUpdated', count);
+    socket.on('sendMessage', (message, callback) => {
+        // broadcast to all connections
+        io.emit('message', message);
+        callback('Delivered!');
+    });
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');
     });
 });
-
-
 
 server.listen(port, () => {
     console.log("App started on port " + port);
